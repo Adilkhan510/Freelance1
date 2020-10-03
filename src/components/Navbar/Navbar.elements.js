ce8e8314c7e35f421034511e902a43a46400f066
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2020-10-03 03:04:32 -0700

    Ak development

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { Container } from '../../globalStyles'
 import { DiAppstore } from 'react-icons/di'
 
+import { Button } from '../../globalStyles'
+
 export const Nav = styled.nav`
 background : #101522;
 height : 80px;
@@ -18,7 +20,8 @@ align-items : center
 export const NavbarContainer = styled(Container)`
 display : flex;
 justify-content : space-between;
-height : 80px
+height : 80px;
+${Container}
 `
 export const NavLogo = styled(Link)`
 color : #fff;
@@ -75,7 +78,7 @@ height : 80px;
 border-bottom : 2px solid transparent;
 &:hover {
     border-bottom : 2px solid #4b59f7;
-}
+};
 
 @media screen and (max-width : 960px) {
     width : 100%;
@@ -100,9 +103,36 @@ height : 100%;
     width : 100%;
     display : table;
 
-    &hover {
+    &:hover {
         color : #4b59f7;
         transition : all 0.3s ease
     }
 }
+`
+
+export const LogoText = styled.p`
+font-family : 'Dancing Script', cursive;
+font-size : 1.2rem
+`
+export const NavItmBtn = styled.li`
+@media screen and (max-width : 960px){
+    display : flex;
+    justify-content : center;
+    align-items : center;
+    width : 100%;
+    height : 120px;
+
+}
+`
+
+export const NavBtnLink = styled(Link)`
+display : flex;
+justify-content : center;
+align-items : center;
+text-decoration :none;
+padding : 8px 16px;
+height : 100%;
+width : 100%;
+border : none;
+outline : none
 `
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,75 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { FaBars, FaTimes } from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, NavIcon, HamburgerIcon, NavMenu, NavLinks, NavItem } from './Navbar.elements'
+import { IconContext } from 'react-icons/lib'
+import { Nav, NavbarContainer, NavLogo, NavIcon, HamburgerIcon, NavMenu, NavLinks, NavItem, LogoText, NavItmBtn, NavBtnLink } from './Navbar.elements'
+
+import { Button } from '../../globalStyles'
 const Navbar = () => {
     const [click, setClick] = useState(false);
+    const [btn, setBtn] = useState(true)
     const handleClick = () => setClick(!click)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setBtn(false)
+        } else {
+            setBtn(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton)
     return (
         <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to='/'>
-                        <NavIcon />
-                        Ultra design
-                    </NavLogo>
-                    <HamburgerIcon onClick={handleClick}>
-                        {
-                            click ? <FaTimes /> : <FaBars />
-                        }
-                    </HamburgerIcon>
-                    <NavMenu onClick={handleClick} click={click}>
-                        <NavItem>
-                            <NavLinks to='/'>Home</NavLinks>
-                        </NavItem>
-                        <NavItem>
-                            <NavLinks to='/'>Services</NavLinks>
-                        </NavItem>
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
+            <IconContext.Provider value={{ color: "#fff" }}>
+
+
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to='/'>
+                            <NavIcon />
+                            <LogoText>
+                                Ak development
+                        </LogoText>
+
+                        </NavLogo>
+                        <HamburgerIcon onClick={handleClick}>
+                            {
+                                click ? <FaTimes /> : <FaBars />
+                            }
+                        </HamburgerIcon>
+                        <NavMenu onClick={handleClick} click={click}>
+                            <NavItem>
+                                <NavLinks to='/'>Home</NavLinks>
+                            </NavItem>
+                            <NavItem>
+                                <NavLinks to='/'>Services</NavLinks>
+                            </NavItem>
+                            <NavItmBtn>
+                                {
+                                    btn ? (
+                                        <NavBtnLink to="/sign-up">
+                                            <Button primary>
+                                                SIGN UP
+                                            </Button>
+                                        </NavBtnLink>
+                                    ) : (
+                                            <NavBtnLink to="/sign-up">
+                                                <Button primary fontBig>
+                                                    SIGN UP
+                                                </Button>
+                                            </NavBtnLink>
+                                        )
+                                }
+                            </NavItmBtn>
+                        </NavMenu>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
     )
 }
